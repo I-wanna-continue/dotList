9fa291d087a2884111444a973792ab4ba675cb09
magralol <gnorr_92@hotmail.com>
2015-04-10 21:00:50 +0200

    some more test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,25 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+    
+    $scope.input = "gregekgkaga"
+    $scope.takeAImage = function(){
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL
+        });
+
+        function onSuccess(imageData) {
+            var image = document.getElementById('myImage');
+            image.src = "data:image/jpeg;base64," + imageData;
+        }
+        
+        function onFail(message) {
+            alert('Failed because: ' + message);
+        }
+
+    }
+    
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
