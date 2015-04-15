375444151ec7a819f39d275ea3631c9f158d1496
piilzner <nils.lofgren@hotmail.com>
2015-04-15 13:14:27 +0200

    Splash, lv height

--- www/js/ctrl/listCtrl.js
+++ www/js/ctrl/listCtrl.js
@@ -6,7 +6,7 @@ app.controller('listCtrl', ['$scope', function ($scope) {
 	$scope.list = [];
 
 	$scope.listTitle = "";
-    //$scope.serchword = "";
+    $scope.serchword = "";
 
     
   $scope.menuHandler = "menu-bar";
